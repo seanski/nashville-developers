1f50bd2ca3d44a465df9c749eb45c3aa367adefc
Sean Hill <seandhi@gmail.com>
2014-02-20 16:09:18 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NashvilleDevelopers::Application.config.secret_key_base = '7977d8f0c7e88468cb743851c3c7480e55a084839e730dcd2b241745cf6770e04c8ea177153b4a21d40bdac52ab29a2d1600948288143e322afb059bb3cc9b81'
