2756fc5a8804041e638702edab77e0cb46ff8d54
Sean Hill <seandhi@gmail.com>
2014-07-17 19:30:59 -0500

    Rails 4 requires that scopes be wrapped in lambdas

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -1,9 +1,9 @@
 class Authorization < ActiveRecord::Base
   belongs_to :user
 
-  scope :facebook, where(provider: :facebook)
-  scope :twitter, where(provider: :twitter)
-  scope :with_user, includes(:user)
+  scope :facebook, -> { where(provider: :facebook) }
+  scope :twitter, -> { where(provider: :twitter) }
+  scope :with_user, -> { includes(:user) }
   scope :for_user, ->(user) { where(user_id: user) }
   scope :for_uid, ->(provided_id) { where(provided_id: provided_id) }
 
